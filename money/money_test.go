f7bb011da6ba3d580bad8c654409339b2c4075ee
Pocket7878 <poketo7878@gmail.com>
2022-03-30 09:39:48 +0900

    spec: UpdateTestMultiplication to expected Times interface. RED

--- money/money_test.go
+++ money/money_test.go
@@ -6,8 +6,13 @@ import (
 
 func TestMultiplication(t *testing.T) {
 	five := NewDollar(5)
-	five.Times(2)
-	if five.Amount() != 10 {
-		t.Fatalf("Expected 10, got %d", five.Amount())
+	product := five.Times(2)
+	if product.Amount() != 10 {
+		t.Fatalf("Expected 10, got %d", product.Amount())
+	}
+
+	product = five.Times(3)
+	if product.Amount() != 15 {
+		t.Fatalf("Expected 15, got %d", product.Amount())
 	}
 }
