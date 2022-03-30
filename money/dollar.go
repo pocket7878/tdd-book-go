6123daaebf2b25a6c7a35cd65d2d42762f1ba026
Pocket7878 <poketo7878@gmail.com>
2022-03-30 09:33:59 +0900

    feature: Add dummy implementation of Amount. first GREEN

--- money/dollar.go
+++ money/dollar.go
@@ -1,14 +1,16 @@
 package money
 
-type Dollar struct{}
+type Dollar struct {
+	amount int
+}
 
 func NewDollar(amount int) *Dollar {
-	return &Dollar{}
+	return &Dollar{10}
 }
 
 func (d *Dollar) Times(multiplier int) {
 }
 
 func (d *Dollar) Amount() int {
-	return 0
+	return d.amount
 }
